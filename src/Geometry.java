d6a3d5a5ed45ff4df0f45647e8ee640928069fa7
Markus Laubenthal <mlaubenthalmail@gmail.com>
2018-12-20 19:46:26 +0100

    fixed a bug with modulo

--- src/Geometry.java
+++ src/Geometry.java
@@ -82,7 +82,7 @@ class Geometry {
     upper.addVertex(v);
     lower.addVertex(v);
     for(int i = 1; true; i++) {
-      int index = (neckStart + i) % outline.size();
+      int index = Math.floorMod((neckStart + i), outline.size());
       v = outline.get(index);
 
       upper.addVertex(v);
@@ -95,7 +95,7 @@ class Geometry {
     }
 
     for(int i = -1; true; i--) {
-      int index = (neckStart + i) % outline.size();
+      int index = Math.floorMod((neckStart + i), outline.size());
       v = outline.get(index);
 
       lower.addVertex(v);
